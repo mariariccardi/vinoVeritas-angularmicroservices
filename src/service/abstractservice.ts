d5871223955d60e7ade4321f3a623fba2f6ec75b
Maria <mariellariccardi06@gmail.com>
2019-11-04 11:47:33 +0100

    modifica1

--- src/service/abstractservice.ts
+++ src/service/abstractservice.ts
@@ -3,30 +3,37 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export abstract class AbstractService<DTO> implements Service<DTO> {
+
     type: string;
     port: string = '8080';
 
-    constructor(protected http: HttpClient){
+    constructor(protected http: HttpClient) {
     }
 
     getAll(): Observable<DTO[]> {
-        return this.http.get<DTO[]>('http://localhost:'+ this.port + '/' + this.type + '/getAll');
+        let heads = {'Authorization': 'Bearer ' + localStorage.getItem('id_token')};
+        return this.http.get<DTO[]>('http://localhost:' + this.port + '/' + this.type, {headers: heads});
     }
 
-    read(id:number): Observable<DTO>{
-        return this.http.get<DTO>('http://localhost:' + this.port + '/' + this.type + '/read?id=' + id);
+    read(id: number): Observable<DTO> {
+        let heads = {'Authorization': 'Bearer ' + localStorage.getItem('id_token')};
+        return this.http.get<DTO>('http://localhost:' + this.port + '/' + this.type + '/' + id, {headers: heads});
     }
 
-    insert(dto:DTO): Observable<any>{
-        return this.http.post('http://localhost:' + this.port + '/' + this.type + '/insert', dto);
+    delete(id: number): Observable<any> {
+        let heads = {'Authorization': 'Bearer ' + localStorage.getItem('id_token')};
+        return this.http.delete('http://localhost:' + this.port + '/' + this.type + '/' + id, {headers: heads});
     }
 
-    update(dto:DTO): Observable<any>{
-        return this.http.put('http://localhost:' + this.port + '/' + this.type + '/update', dto);
+    insert(dto: DTO): Observable<any> {
+        let heads = {'Authorization': 'Bearer ' + localStorage.getItem('id_token')};
+        return this.http.post('http://localhost:' + this.port + '/' + this.type, dto, {headers: heads});
     }
 
-    delete(id:number): Observable<any>{
-        return this.http.delete('http://localhost:' + this.port + '/' + this.type + '/delete?id=' + id);
+    update(dto: DTO): Observable<DTO> {
+        let heads = {'Authorization': 'Bearer ' + localStorage.getItem('id_token')};
+        return this.http.put<DTO>('http://localhost:' + this.port + '/' + this.type, dto, {headers: heads});
+
     }
 
 }
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -9,14 +9,35 @@ import { AbstractService } from './abstractservice';
     providedIn: 'root'
 })
 
-export class UserService extends AbstractService<UserDTO>{
-    constructor(http:HttpClient){
-        super(http);
-        this.type= 'user';
-    }
+export class UserService extends AbstractService<UserDTO> {
 
-    login(loginDTO:LoginDTO): Observable<UserDTO>{
+  constructor(http: HttpClient) {
+    super(http);
+    this.type = 'api/users';
+  }
 
-        return this.http.post<any>('http://localhost:8080/'+ this.type + '/login', loginDTO)
-    }
+  login(loginDTO: LoginDTO): Observable<any> {
+    return this.http.post<any>('http://localhost:8080/api/authenticate', loginDTO);
+  }
+
+  getAccount(): Observable<UserDTO> {
+    let heads = {'Authorization': 'Bearer ' + localStorage.getItem('id_token')};
+    return this.http.get<any>('http://localhost:8080/api/account', {headers: heads});
+  }
+
+  readUser(login: string): Observable<UserDTO> {
+    let heads = {'Authorization': 'Bearer ' + localStorage.getItem('id_token')};
+    return this.http.get<UserDTO>('http://localhost:' + this.port + '/' + this.type + '/' + login, {headers: heads});
+  }
+
+  deleteUser(login: string): Observable<any> {
+    let heads = {'Authorization': 'Bearer ' + localStorage.getItem('id_token')};
+    return this.http.delete('http://localhost:' + this.port + '/' + this.type + '/' + login, {headers: heads});
+  }
+
+  insert(dto: UserDTO): Observable<any> {
+    let heads = {'Authorization': 'Bearer ' + localStorage.getItem('id_token')};
+    return this.http.post('http://localhost:' + this.port + '/api/register', dto, {headers: heads});
+  }
+
 }
